1a89854b1b14556802d36e8df36775c7db229248
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 19:18:21 -0800

    Upgrade modules

--- src/worker.ts
+++ src/worker.ts
@@ -1,33 +1,47 @@
 import spawn, { crossSpawn, type SpawnResult } from 'cross-spawn-cb';
+import eos from 'end-of-stream';
 import Queue from 'queue-cb';
-
-import Transform from './Transform';
+import concatWritable from './concatWritable';
 import nextColor from './nextColor';
+import prefixTransform from './prefixTransform';
 
 import type { SpawnOptions, StreamingOptions } from './types';
 
-export default function spawnStreaming(command: string, args: string[], spawnOptions_: SpawnOptions, options: StreamingOptions, callback) {
-  const spawnOptions = { ...spawnOptions_ };
-  spawnOptions.encoding = 'utf8';
-  if (spawnOptions.stdio === 'inherit') delete spawnOptions.stdio;
-  const cp = crossSpawn(command, args, spawnOptions);
+function pipeline(input, output, options, color) {
+  if (options.prefix) return input.pipe(prefixTransform(options.prefix, color)).pipe(output);
+  return input.pipe(output);
+}
 
-  const color = nextColor();
+export default function spawnStreaming(command: string, args: string[], spawnOptions: SpawnOptions, options: StreamingOptions, callback) {
+  const { encoding, stdio, ...csOptions } = spawnOptions;
+  const cp = crossSpawn(command, args, csOptions);
+  const color = options.prefix ? nextColor() : null;
   const outputs = { stdout: null, stderr: null };
-  if (cp.stdout) outputs.stdout = new Transform(cp.stdout, process.stdout, spawnOptions_, options, color);
-  if (cp.stderr) outputs.stderr = new Transform(cp.stderr, process.stderr, spawnOptions_, options, color);
 
   const queue = new Queue();
-  !outputs.stdout || queue.defer(outputs.stdout.collect.bind(outputs.stdout));
-  !outputs.stderr || queue.defer(outputs.stderr.collect.bind(outputs.stderr));
-  queue.defer(spawn.worker.bind(null, cp, spawnOptions));
+  if (cp.stdout) {
+    if (stdio === 'inherit') pipeline(cp.stdout, process.stdout, options, color);
+    else {
+      outputs.stdout = concatWritable((output) => {
+        outputs.stdout.output = output.toString(encoding || 'utf8');
+      });
+      queue.defer(eos.bind(null, pipeline(cp.stdout, outputs.stdout, options, color)));
+    }
+  }
+  if (cp.stderr) {
+    if (stdio === 'inherit') pipeline(cp.stderr, process.stderr, options, color);
+    else {
+      outputs.stderr = concatWritable((output) => {
+        outputs.stderr.output = output.toString(encoding || 'utf8');
+      });
+      queue.defer(eos.bind(null, pipeline(cp.stderr, outputs.stderr, options, color)));
+    }
+  }
+  queue.defer(spawn.worker.bind(null, cp, { ...csOptions, encoding: 'utf8' }));
   queue.await((err) => {
-    outputs.stdout.drain();
-    outputs.stderr.drain();
-
     const res = (err ? err : {}) as SpawnResult;
-    res.stdout = outputs.stdout.output();
-    res.stderr = outputs.stderr.output();
+    res.stdout = outputs.stdout ? outputs.stdout.output : null;
+    res.stderr = outputs.stderr ? outputs.stderr.output : null;
     res.output = [res.stdout, res.stderr, null];
     err ? callback(err) : callback(null, res);
   });
