b4700d6deabd76bb4b2299607bfccca2859175e6
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 19:18:21 -0800

    Replace Transform with prefixTransform and use readable-stream

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,6 @@ export type { SpawnOptions, SpawnCallback, SpawnResult } from 'cross-spawn-cb';
 
 export interface StreamingOptions {
   prefix?: string;
-  throttle?: number;
 }
 
 export type ColorFunction = (s: string) => string;
