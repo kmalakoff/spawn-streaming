0250b2624a0959a4a0e2487f3f75f119cfebbf8b
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-18 06:19:59 -0800

    Remove throttle and move internal modules to lib/

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,11 +1,8 @@
 import assert from 'assert';
 import spawnStreaming from 'spawn-streaming';
-import { Transform, nextColor } from 'spawn-streaming';
 
 describe('exports .mjs', () => {
   it('defaults', () => {
     assert.equal(typeof spawnStreaming, 'function');
-    assert.equal(typeof Transform, 'function');
-    assert.equal(typeof nextColor, 'function');
   });
 });
--- test/exports/require.test.cjs
+++ test/exports/require.test.cjs
@@ -1,11 +1,8 @@
 const assert = require('assert');
 const spawnStreaming = require('spawn-streaming');
-const { Transform, nextColor } = require('spawn-streaming');
 
 describe('exports .cjs', () => {
   it('defaults', () => {
     assert.equal(typeof spawnStreaming, 'function');
-    assert.equal(typeof Transform, 'function');
-    assert.equal(typeof nextColor, 'function');
   });
 });
