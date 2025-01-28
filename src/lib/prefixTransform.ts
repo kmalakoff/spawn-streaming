e713e7c2dde2d4d2c795d65a45ae2cb993e58864
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-28 06:57:23 -0800

    Rename regEx to REGEX_NEW_LINE in prefixTransform

--- src/lib/prefixTransform.ts
+++ src/lib/prefixTransform.ts
@@ -2,7 +2,7 @@ import c from 'colors';
 import { Transform } from 'readable-stream';
 import type { ColorFunction } from '../types';
 
-const regEx = /\r\n|[\n\v\f\r\x85\u2028\u2029]/g;
+const REGEX_NEW_LINE = /\r\n|[\n\v\f\r\x85\u2028\u2029]/g;
 
 export default function prefixTransform(prefix: string, color: ColorFunction) {
   let last = '';
@@ -12,7 +12,7 @@ export default function prefixTransform(prefix: string, color: ColorFunction) {
   return new Transform({
     transform(chunk, _enc, callback) {
       const more = last + chunk.toString('utf8');
-      const lines = more.split(regEx);
+      const lines = more.split(REGEX_NEW_LINE);
       last = lines.pop();
       lines.forEach((line) => this.push(createLine(line)));
       callback();
