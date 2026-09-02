05f0c62bd243c18e4731ddc07e7700022cafc2cd
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:47:43 -0700

    Upgrade to mandatory typescript extensions and modules

--- src/lib/nextColor.ts
+++ src/lib/nextColor.ts
@@ -3,7 +3,7 @@ import c from 'colors';
 const colors = ['cyan', 'magenta', 'blue', 'yellow', 'green', 'red', 'brightCyan', 'brightMagenta', 'brightBlue', 'brightYellow', 'brightGreen', 'brightRed'];
 let colorIndex = 0;
 
-import type { ColorFunction } from '../types';
+import type { ColorFunction } from '../types.js';
 
 export default function nextColor(): ColorFunction {
   const colorName = colors[colorIndex % colors.length];
