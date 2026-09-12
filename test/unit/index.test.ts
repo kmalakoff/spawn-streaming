a63e694f35d7d62180eadfae87c49462b3b417af
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 18:23:44 -0700

    Upgrade modules

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,11 +1,14 @@
 import assert from 'assert';
 import isVersion from 'is-version';
 import Pinkie from 'pinkie-promise';
+import type { SpawnResult } from 'spawn-streaming';
 import spawnStreaming from 'spawn-streaming';
 import getLines from '../lib/getLines.ts';
 
 const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE ?? '');
 const NODE = isWindows ? 'node.exe' : 'node';
+const PLAIN_JS_ENV = { ...process.env };
+delete PLAIN_JS_ENV.NODE_OPTIONS;
 
 describe('index', () => {
   (() => {
@@ -50,6 +53,18 @@ describe('index', () => {
       done();
     });
   });
+
+  it('resolves a Promise after a child process succeeds', () =>
+    (spawnStreaming(NODE, ['-e', "process.stdout.write('promise')"], { encoding: 'utf8', env: PLAIN_JS_ENV }) as Promise<SpawnResult>).then((res) => {
+      assert.equal(res.stdout, 'promise');
+      assert.equal(res.stderr, '');
+    }));
+
+  it('rejects a Promise after a child process fails', () =>
+    (spawnStreaming(NODE, ['-e', 'process.exit(1)'], { encoding: 'utf8', env: PLAIN_JS_ENV }) as Promise<SpawnResult>).then(
+      () => assert.fail('Expected the Promise to reject'),
+      (err: Error) => assert.ok(/Non-zero exit code: 1/.test(err.message))
+    ));
 
   it('throws when stdio inherit and encoding are both specified', () => {
     assert.throws(() => {
